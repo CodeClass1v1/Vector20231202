b70297076a6aa1b4093a1bde7f70da3690ea390b
Vector554 <554052899@qq.com>
2023-12-20 00:34:44 +0900

    asaaaaa

--- py1/assignment1.py
+++ py1/assignment1.py
@@ -1,4 +1,12 @@
-a = 0
-b = 0
-b = 0
-
+"""
+Author: ye wente
+Assignment / Part: HW1 - Q1 (etc.)
+Date due: 2023-02-09, 11:59pm
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+Bornforthis 1v1 School of Computer Science and Procedures on
+Academic Misconduct.
+"""
+name = input("What is your name? ")
+age= input("How old are you? ")
+print(name+','+age+" is taking this course")
